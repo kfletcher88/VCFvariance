371d2fa5ab486adef4ab0711416ae048586b17eb
Kyle Fletcher <kfletcher@ucdavis.edu>
2021-09-08 16:48:00 -0700

    Add R code for allele bar chart, implement -B to provide option for plotting

---
+++ R/Ploidy_v2.R
@@ -0,0 +1,8 @@
+args <- commandArgs(trailingOnly = TRUE)
+a <- scan(args[1], what=numeric(), sep=' ', quiet=TRUE)
+b <- paste0(args[1],".jpg")
+lab<-c(0.25, 0.5, 0.75)
+jpeg(b)
+m<-barplot(table(factor(a, seq(0.2,0.8,0.01))),xlab="",xaxt="n",las=1)
+axis(1,at=m[c(5, 30, 55)],labels=lab, cex.axis=1)
+graphics.off()
